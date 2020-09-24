32ede4ada38f2d1a757ece91a3c66b6a18112b1b
bouali zakariae <bouali_zakariae@hotmail.fr>
2020-09-24 10:52:09 +0200

    this inside an arrow function

--- js/fundamentals/this.js
+++ js/fundamentals/this.js
@@ -1,5 +1,5 @@
 //if running from nodejs environement, this reference the global object
-//if it is running inside a browser, this will reference the Windows object 
+//if it is running inside a browser, this will reference the Window object 
 function add(){
     console.log(this);
 }
@@ -9,6 +9,7 @@ add()
 console.log("still this will refer the global scope");
 function minus(){
     function innerfunction(){
+        // keep in mind, if we use 'use strict', this will be undefined and will no longer reference the window object  
         console.log(this)
     }
     innerfunction();
@@ -25,22 +26,26 @@ let person = {
         return this.age;
     }
 }
-
 console.log(person);
 console.log(person.getage());
 
 
+// in this example, this will reference the window object and not the person object.
+// as window dosen't have an age variable, undefined is returned.
 console.log("let's see something else");
 agefunction = person.getage;
 console.log(agefunction());
 
-// in the above example, this will reference the windows object and not the person object.
-// as windows dosen't have an age variable, undefined is returned.
-
-
-
-
-
-
-
-
+console.log("another case with arrow function inside a function");
+let person2 = {
+    name: 'brahim',
+    age: 30,
+    getage : function  (){
+        return ()=> {
+            console.log(this);
+            return this.age;
+        }
+    }  
+}
+agefunction2 = person2.getage();
+console.log(agefunction2());
